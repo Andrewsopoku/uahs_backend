bf4353ee8a3cd3f96fc65b96b01c6e2a9b285ad1
andrewsopoku <andrewsopoku6@gmail.com>
2019-10-30 11:28:49 +0000

    Ambulance type added to form

--- core/forms/ambulance_form.py
+++ core/forms/ambulance_form.py
@@ -1,6 +1,9 @@
 
 from django import forms
 
+from core.models.ambulance import amb_type
+
+
 class NewAmbulanceForm(forms.Form):
     registration_number = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
         'class': "form-control mb-4 input-rounded" ,'placeholder' :'Registration Number'}), )
@@ -14,3 +17,6 @@ class NewAmbulanceForm(forms.Form):
     make_year = forms.CharField(max_length=255, required=False,
                               widget=forms.TextInput
                                   (attrs={'class': "form-control mb-4 input-rounded" ,'placeholder' :'Make Year'}), )
+    type = forms.ChoiceField(choices=amb_type,widget=forms.Select(
+        attrs={'class': "form-control mb-4 input-rounded searchable", 'searchable': "Search here.."}), )
+
